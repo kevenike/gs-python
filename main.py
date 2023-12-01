5209672221070fa6885e837f858d252ef7a28bb4
murillo <murilloferreira348@gmail.com>
2023-12-01 08:59:22 -0300

    Implementa a função de cadastro de pacientes com validação de entrada

--- main.py
+++ main.py
@@ -10,3 +10,13 @@ def exibir_menu_principal():
     print("2. Agendar Consulta")
     print("3. Visualizar Lista de Pacientes")
     print("4. Sair" + Style.RESET_ALL)
+
+    # Função para cadastrar um paciente
+def cadastrar_paciente():
+    print("\nCadastro de Paciente:")
+    nome = input("Digite o nome do paciente: ")
+    idade = validar_numero("Digite a idade do paciente: ")
+    documento = validar_documento("Digite o número do documento do paciente: ")
+    paciente = {"Nome": nome, "Idade": idade, "Documento": documento}
+    lista_pacientes.append(paciente)
+    print("Paciente cadastrado com sucesso!")
